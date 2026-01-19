ac80ed0143246ca9fc17d920f74ce757d6d9aaea
Valentin Nuñez <127317122+valentin21103@users.noreply.github.com>
2026-01-19 12:26:20 -0300

    implementacion de Modales SweetAlerts y algunas mejoras

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ListaMedicamentosComponent } from './components/lista-medicamentos/lista-medicamentos.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,ListaMedicamentosComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
